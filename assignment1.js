29b1a326f4793ca715f1e2f1e4b7c8b796a20cef
JEFFDEV-01 <guosidoubra@gmail.com>
2024-09-03 18:18:42 -0700

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,21 +1,21 @@
 // function to calculate arithemic
-const calculate = (num1, num2, operation) => {
+const calculate = (number1, number2, operation) => {
     // Validate that the input numbers are not null or undefined
-    if (num1 == null || num2 == null) return "Invalid input numbers";
+    if (number1 == null || number2 == null) return "Invalid input numbers";
 
     // Perform the appropriate operation based on the input string
     switch (operation) {
         case 'add':
-            return num1 + num2;
+            return number1 + number2;
         case 'subtract':
-            return num1 - num2;
+            return number1 - number2;
         case 'multiply':
-            return num1 * num2;
+            return number1 * number2;
         case 'divide': 
-             if(num2 === 0 ) {
+             if(number2 === 0 ) {
                 return "Cannot divide by zero" 
              }
-            else { return num1 / num2
+            else { return number1 / number2
             };
         default:
             return "Invalid operation";
